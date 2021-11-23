205d34a58832e2d8eb5c33d8fcb10c6c06f490ea
1savita <94592743+1savita@users.noreply.github.com>
2021-11-23 17:18:31 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.color("red")
+turtle.pensize(5)
+side=150
+turtle.forward(side)
+turtle.left(120)
+
+turtle.forward(side)
+turtle.left(120)
+
+turtle.forward(side)
+turtle.left(120)
